1fb2d44811eb1d52228fdd2468c4c6256821b5c2
Andrea F. Daniele <afdaniele@ttic.edu>
2019-09-21 14:00:15 -0500

    updated launchfile

--- launch.sh
+++ launch.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 set -e
+source /environment.sh
 
-CODE_DIR="$( cd "$( dirname "${LAUNCHFILE}" )" >/dev/null 2>&1 && pwd )"
+# initialize launch file
+dt_launchfile_init
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 
+
 # NOTE: Use the variable CODE_DIR to know the absolute path to your code
 
 echo "This is an empty launch script. Update it to launch your application."
+
+
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
+
+# terminate launch file
+dt_launchfile_terminate
